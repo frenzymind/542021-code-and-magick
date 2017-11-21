c87960e25dc544e4dec45ac1653b68eed2f96fac
Николай Данченко <frenzymind@mail.ru>
2017-11-21 11:30:46 +0800

    Добавлена переменная style

--- js/stat.js
+++ js/stat.js
@@ -18,6 +18,8 @@ window.renderStatistics = function(ctx, names, times) {
     var offSetTextY = 30;
     var titleTextLevel1 = 'Ура вы победили!';
     var titleTextLevel2 = 'Список результатов:';
+    var lineParagraphFactor = 1.6;
+    var style = 'black';
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
     ctx.fillRect(x + offSet, y + offSet, width, heigh);
@@ -28,9 +30,10 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.font = '16px PT Mono';
     //ctx.fillStyle = 'black';
     //ctx.fillText(titleTextLevel1, x + offSetTextX, y + offSetTextY);
-    drawText(titleTextLevel1, x + offSetTextX, y + offSetTextY, ctx, 'black');
+    drawText(titleTextLevel1, x + offSetTextX, y + offSetTextY, ctx, style);
 
-    ctx.fillText(titleTextLevel2, x + offSetTextX, y + offSetTextY * 1.6);
+    //ctx.fillText(titleTextLevel2, x + offSetTextX, y + offSetTextY * 1.6);
+    drawText(titleTextLevel2, x + offSetTextX, y + offSetTextY * lineParagraphFactor, ctx, style);
 
     var gistStartX = 150;
     var gistStartY = 240;
@@ -43,21 +46,24 @@ window.renderStatistics = function(ctx, names, times) {
     for (var i = 0; i < names.length; i++) {
 
       if (names[i] === 'Вы') {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+        style = 'rgba(255, 0, 0, 1)';
       }
       else {
-        ctx.fillStyle = 'rgba(0, 30, 255,'+ Math.random() +')';
+        style = 'rgba(0, 30, 255,'+ Math.random() +')';
       }
 
       var posX = gistStartX + (gistWidth + gistDistance) * i;
       var max = Math.max.apply(null, times);
       var gistValue = Math.round(times[i] * gistHeight / max);
 
+      ctx.fillStyle = style;
       ctx.fillRect(posX, gistStartY, gistWidth, -gistValue);
 
-      ctx.fillStyle = 'black';
-      ctx.fillText(names[i], posX, gistStartY + gistTextBotOffSet);
-      ctx.fillText(Math.round(times[i]), posX, gistStartY - gistValue + gistTextTopOffSet);
+      style = 'black';
+      //ctx.fillText(names[i], posX, gistStartY + gistTextBotOffSet);
+      drawText(names[i], posX, gistStartY + gistTextBotOffSet, ctx, style);
+      //ctx.fillText(Math.round(times[i]), posX, gistStartY - gistValue + gistTextTopOffSet);
+      drawText(Math.round(times[i]), posX, gistStartY - gistValue + gistTextTopOffSet, ctx, style);
     }
 
  }
