c448b2371990405e2d738cdf702a6be0b3301606
Николай Данченко <frenzymind@mail.ru>
2017-11-21 12:34:56 +0800

    Исправление Travis CI ошибок #3

--- js/stat.js
+++ js/stat.js
@@ -16,7 +16,7 @@ function drawRect(x, y, width, height, ctx, style) {
   return;
 }
 
-window.renderStatistics = function( ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
 
   var x = 100;
   var y = 10;
@@ -69,4 +69,5 @@ window.renderStatistics = function( ctx, names, times) {
     drawText(names[i], posX, gistStartY + gistTextBotOffSet, ctx, style);
     drawText(Math.round(times[i]), posX, gistStartY - gistValue + gistTextTopOffSet, ctx, style);
   }
+
 }
